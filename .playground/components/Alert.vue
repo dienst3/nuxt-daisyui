62ccced273e24ff733bc8d058e1f7e2bd24fc17b
Dany Sluijk <me@dany.dev>
2023-04-01 20:26:29 +0200

    feat: add alert component

---
+++ .playground/components/Alert.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Alert</h2>
+  <DaisyAlert type="info" content="I am an informational alert!" />
+  <DaisyAlert type="success" content="I am a success alert!" />
+  <DaisyAlert type="warning" content="I am a warning alert!" />
+  <DaisyAlert type="error" content="I am a error alert!" />
+</template>
---
+++ components/Daisy/Alert.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="content && !hidden" class="alert shadow-lg my-3" :class="classes">
+    <div>
+      <Icon :name="icon" size="24px" />
+      <span>{{ content }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  type: "info" | "success" | "warning" | "error";
+  hidden?: boolean;
+  content?: string;
+}
+
+const props = defineProps<Props>();
+
+const icon = computed(() => {
+  switch (props.type) {
+    case "info":
+      return "material-symbols:info-outline";
+    case "success":
+      return "material-symbols:check-circle-outline";
+    case "warning":
+      return "material-symbols:warning-outline";
+    case "error":
+      return "material-symbols:cancel-outline";
+  }
+});
+
+const classes = computed(() => ({
+  "alert-info": props.type === "info",
+  "alert-success": props.type === "success",
+  "alert-warning": props.type === "warning",
+  "alert-error": props.type === "error",
+}));
+</script>
