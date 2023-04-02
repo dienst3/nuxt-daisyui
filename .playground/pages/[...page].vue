9c86993de9d32f72d2ca63b531976999befdc2c8
Dany Sluijk <me@dany.dev>
2023-04-02 16:38:30 +0200

    feat(tabs): add NuxtLink compatible component

---
+++ .playground/pages/[...page].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="max-w-screen-md mx-auto pt-4 md:pt-8 px-4 mb-2 prose">
+    <Button />
+    <Alert />
+    <InputText />
+    <InputToggle />
+    <InputSelect />
+    <Tabs />
+    <Tooltip />
+    <Collapse />
+    <Table />
+  </div>
+</template>
