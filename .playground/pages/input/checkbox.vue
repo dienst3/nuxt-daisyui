c2facc3a5d732f68fbcebfdb576ee21c6b58182e
Dany Sluijk <me@dany.dev>
2023-08-29 17:41:36 +0200

    docs(checkbox): add interactive docs

--- .playground/pages/input/checkbox.vue
+++ .playground/pages/input/checkbox.vue
@@ -1,14 +1,55 @@
 <template>
-  <h1>Checkbox</h1>
-  <DaisyInputCheckbox />
-  <DaisyInputCheckbox
-    label="I'm disabled and checked"
-    disabled
-    :model-value="true"
-  />
-  <DaisyInputCheckbox
-    label="I don't know what I am"
-    :model-value="'indeterminate'"
-  />
-  <DaisyInputCheckbox label="Click me!" color="primary" size="lg" />
+  <PageTitle>Checkbox</PageTitle>
+  <div class="flex flex-col 2xl:flex-row gap-8">
+    <PropsTable>
+      <PropsTableEntry name="label" description="The label of the checkbox.">
+        <DaisyInputText
+          v-model="label"
+          placeholder="Add a label!"
+          size="sm"
+          bordered
+        />
+      </PropsTableEntry>
+      <PropsTableEntry name="disabled" description="Disable the checkbox.">
+        <DaisyInputToggle v-model="disabled" size="sm" />
+      </PropsTableEntry>
+      <PropsTableEntry name="size" description="Set the size of the checkbox.">
+        <PropsTableCommonSize v-model="size" />
+      </PropsTableEntry>
+      <PropsTableEntry
+        name="color"
+        description="Set the color of the checkbox."
+      >
+        <PropsTableCommonColor v-model="color" />
+      </PropsTableEntry>
+    </PropsTable>
+    <Preview :code="previewCode">
+      <DaisyInputCheckbox
+        :label="label"
+        :disabled="disabled"
+        :size="size"
+        :color="color"
+      />
+    </Preview>
+  </div>
 </template>
+
+<script setup lang="ts">
+const label = ref();
+const disabled = ref(false);
+const size = ref();
+const color = ref();
+
+const previewCode = computed(() => {
+  const attrs = useComponentAttrs({
+    "v-model": ref("model"),
+    label,
+    disabled,
+    size,
+    color,
+  });
+
+  return `\
+<DaisyInputCheckbox${attrs.value} />`;
+});
+</script>
