455235e7f9815fc9f99cc93619cfab199e4839e4
Dany Sluijk <me@dany.dev>
2023-08-29 18:42:10 +0200

    docs(tab): add interactive docs

--- .playground/pages/navigation/tab.vue
+++ .playground/pages/navigation/tab.vue
@@ -1,18 +1,57 @@
 <template>
-  <h1>Tab</h1>
-  <DaisyTab v-model="activeTab" lifted>
-    <DaisyTabEntry name="one" title="Tab One" />
-    <DaisyTabEntry name="two" title="Tab Two" disabled />
-    <DaisyTabEntry name="three" title="Tab Three" />
-  </DaisyTab>
-  <span>{{ activeTab }}</span>
-  <DaisyTab lifted>
-    <DaisyTabLink to="/" title="Tab One" />
-    <DaisyTabLink to="/two" title="Link Two" disabled />
-    <DaisyTabLink to="/three" title="Tab Three" />
-  </DaisyTab>
+  <PageTitle>Tab</PageTitle>
+  <div class="flex flex-col 2xl:flex-row gap-8">
+    <PropsTable>
+      <PropsTableEntry name="type" description="The type of tabs to use.">
+        <DaisyInputSelect v-model="type" bordered size="sm">
+          <option value="">Default</option>
+          <option value="bordered">Bordered</option>
+          <option value="boxed">Boxed</option>
+          <option value="lifted">Lifted</option>
+        </DaisyInputSelect>
+      </PropsTableEntry>
+      <PropsTableEntry name="size" description="Set the size of the tabs.">
+        <PropsTableCommonSize v-model="size" />
+      </PropsTableEntry>
+    </PropsTable>
+    <Preview :code="previewCode">
+      <DaisyTab v-model="model" :type="type" :size="size">
+        <DaisyTabEntry name="One" title="Tab One" />
+        <DaisyTabEntry name="Two" title="Tab Two" disabled />
+        <DaisyTabEntry name="Three" title="Tab Three" />
+        <DaisyTabLink
+          to="https://dienst3.nl"
+          title="Dienst 3"
+          target="_blank"
+        />
+      </DaisyTab>
+      <span>Current tab: {{ model }}</span>
+    </Preview>
+  </div>
 </template>
 
-<script lang="ts" setup>
-const activeTab = ref("one");
+<script setup lang="ts">
+const model = ref("One");
+const type = ref();
+const size = ref();
+
+const previewCode = computed(() => {
+  const attrs = useComponentAttrs({
+    "v-model": ref("model"),
+    type,
+    size,
+  });
+
+  return `\
+<DaisyTab${attrs.value}>
+  <DaisyTabEntry name="One" title="Tab One" />
+  <DaisyTabEntry name="Two" title="Tab Two" disabled />
+  <DaisyTabEntry name="Three" title="Tab Three" />
+  <DaisyTabLink
+    to="https://dienst3.nl"
+    title="Dienst 3"
+    target="_blank"
+  />
+</DaisyTab>`;
+});
 </script>
