f03f822c20ddf1514d738a12496a77a9870d8262
Dany Sluijk <me@dany.dev>
2023-08-30 12:57:45 +0200

    docs(select): add interactive docs

--- .playground/pages/input/select.vue
+++ .playground/pages/input/select.vue
@@ -1,13 +1,100 @@
 <template>
-  <h1>Select</h1>
-  <DaisyInputSelect>
-    <option value="" disabled>Select Something</option>
-    <option value="one">One</option>
-    <option value="two">Two</option>
-  </DaisyInputSelect>
-  <DaisyInputSelect title="Select me!" color="primary" bordered required>
-    <option value="" disabled>Select Something</option>
-    <option value="one">One</option>
-    <option value="two">Two</option>
-  </DaisyInputSelect>
+  <PageTitle>Select</PageTitle>
+  <div class="flex flex-col 2xl:flex-row gap-8">
+    <PropsTable>
+      <PropsTableEntry
+        name="title"
+        description="The title to show next to the input."
+      >
+        <DaisyInputText
+          v-model="title"
+          placeholder="Some title"
+          size="sm"
+          bordered
+        />
+      </PropsTableEntry>
+      <PropsTableEntry
+        name="error"
+        description="An optional error to show at the input."
+      >
+        <DaisyInputText
+          v-model="error"
+          placeholder="Oh no!"
+          size="sm"
+          bordered
+        />
+      </PropsTableEntry>
+      <PropsTableEntry
+        name="required"
+        description="Style the input to be required."
+      >
+        <DaisyInputToggle v-model="required" size="sm" />
+      </PropsTableEntry>
+      <PropsTableEntry name="disabled" description="Disable the input.">
+        <DaisyInputToggle v-model="disabled" size="sm" />
+      </PropsTableEntry>
+      <PropsTableEntry
+        name="bordered"
+        description="Add a border around the input."
+      >
+        <DaisyInputToggle v-model="bordered" size="sm" />
+      </PropsTableEntry>
+      <PropsTableEntry name="ghost" description="Style the input as a ghost.">
+        <DaisyInputToggle v-model="ghost" size="sm" />
+      </PropsTableEntry>
+      <PropsTableEntry name="size" description="Set the size of the input.">
+        <PropsTableCommonSize v-model="size" />
+      </PropsTableEntry>
+      <PropsTableEntry name="color" description="Set the color of the input.">
+        <PropsTableCommonColor v-model="color" />
+      </PropsTableEntry>
+    </PropsTable>
+    <Preview :code="previewCode">
+      <DaisyInputSelect
+        :title="title"
+        :error="error"
+        :required="required"
+        :disabled="disabled"
+        :bordered="bordered"
+        :ghost="ghost"
+        :size="size"
+        :color="color"
+      >
+        <option value="" disabled>Select Something</option>
+        <option value="one">One</option>
+        <option value="two">Two</option>
+      </DaisyInputSelect>
+    </Preview>
+  </div>
 </template>
+
+<script setup lang="ts">
+const title = ref("Selection title");
+const error = ref();
+const required = ref(false);
+const disabled = ref(false);
+const bordered = ref(false);
+const ghost = ref(false);
+const size = ref();
+const color = ref();
+
+const previewCode = computed(() => {
+  const attrs = useComponentAttrs({
+    title,
+    error,
+    required,
+    disabled,
+    bordered,
+    ghost,
+    size,
+    color,
+  });
+
+  return `\
+<DaisyInputSelect${attrs.value}>
+  <option value="" disabled>Select Something</option>
+  <option value="one">One</option>
+  <option value="two">Two</option>
+</DaisyInputSelect>`;
+});
+</script>
